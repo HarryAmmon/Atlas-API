83645df9c2881c3d3ec224e28d382aec5144058e
HarryAmmon <harry_a99@hotmail.co.uk>
2021-03-14 13:59:50 +0000

    Add unit tests for UserStory

--- code/Atlas-API.Tests/UserStoryTests.cs
+++ code/Atlas-API.Tests/UserStoryTests.cs
@@ -15,21 +15,55 @@ namespace Atlas_API.Tests
         public void Must_Have_ID()
         {
             // Arrange
-            string x = "x";
-        
+            string id = "myID";
+
             // Act
-            UserStory result = new UserStory(x, "title");
-            
+            var result = new UserStory(id, "title");
+
             // Arrange
-            Assert.AreEqual(result.Id, x);
-    
+            Assert.AreEqual(result.Id, id);
         }
 
         [Test]
-        public void Test2()
+        public void Must_Have_Title()
         {
-            Assert.Pass();
+            // Arrange
+            string title = "a title";
 
+            // Act
+            var result = new UserStory("1", title);
+
+            // Arrange
+            Assert.AreEqual(result.Title, title);
+        }
+
+        [Test]
+        public void Story_Points_Optional_Value_Is_Zero()
+        {
+            // Act
+            var result = new UserStory("123", "title");
+
+            // Arrange
+            Assert.AreEqual(result.StoryPoints, 0);
+        }
+
+        [Test]
+        public void Description_Optional_Value_Is_WhiteSpace()
+        {
+            // Act
+            var result = new UserStory("123", "title");
+
+            // Arrange
+            Assert.That(string.IsNullOrWhiteSpace(result.Description));
+        }
+
+        [Test]
+        public void AcceptanceCriteria_Optional_Value_Is_WhiteSpace(){
+            // Act
+            var result = new UserStory("123", "title");
+
+            // Arrange
+            Assert.That(string.IsNullOrWhiteSpace(result.AcceptanceCriteria));
         }
     }
 }
