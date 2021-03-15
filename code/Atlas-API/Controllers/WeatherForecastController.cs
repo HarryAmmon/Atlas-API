e16020ab2abd70b3a21b7e8764b3c99b5af7c052
HarryAmmon <harry_a99@hotmail.co.uk>
2021-03-15 10:39:40 +0000

    PoC for accessing KeyVault

--- code/Atlas-API/Controllers/WeatherForecastController.cs
+++ code/Atlas-API/Controllers/WeatherForecastController.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Azure.Security.KeyVault.Secrets;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Logging;
 
@@ -17,15 +18,21 @@ namespace Atlas_API.Controllers
         };
 
         private readonly ILogger<WeatherForecastController> _logger;
-
-        public WeatherForecastController(ILogger<WeatherForecastController> logger)
+        private readonly SecretClient _client;
+        public WeatherForecastController(ILogger<WeatherForecastController> logger, SecretClient client)
         {
             _logger = logger;
+            _client = client;
+
         }
 
         [HttpGet]
         public IEnumerable<WeatherForecast> Get()
         {
+
+            KeyVaultSecret secret = _client.GetSecret("MyFirstSecret");
+            Console.WriteLine(secret.Value);
+
             var rng = new Random();
             return Enumerable.Range(1, 5).Select(index => new WeatherForecast
             {
